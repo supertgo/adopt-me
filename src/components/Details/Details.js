cba7cf8a976acfab1987a0dfb64b2d8815c96b97
supertgo <thiagoroberto2003@gmail.com>
2021-06-06 12:39:43 -0300

    Adding another 2 components

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,14 +1,10 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
+import Carousel from "../Carousel/Carousel";
+import ErrorBoundary from "../../../ErrorBoundary";
 
 class Details extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      loading: true,
-    };
-  }
+  state = { loading: true };
 
   async componentDidMount() {
     const res = await fetch(
@@ -29,9 +25,19 @@ class Details extends Component {
   render() {
     if (this.state.loading) return <h2>loading ...</h2>;
 
-    const { animal, breed, city, state, description, name } = this.state;
+    const {
+      animal,
+      breed,
+      city,
+      state,
+      description,
+      name,
+      images,
+    } = this.state;
+
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
@@ -43,4 +49,12 @@ class Details extends Component {
   }
 }
 
-export default withRouter(Details);
+const DetailsWithRouter = withRouter(Details);
+
+export default function detailsWithErrorBoundary() {
+  return (
+    <ErrorBoundary>
+      <DetailsWithRouter />
+    </ErrorBoundary>
+  );
+}
